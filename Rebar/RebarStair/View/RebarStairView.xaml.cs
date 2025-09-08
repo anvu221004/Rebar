805837ae6ddd3928b5bc9b12085b9faafd390051
An Tuan Vu-67th2-0197867 <0197867@st.huce.edu.vn>
2025-09-08 22:39:07 +0700

    giaodien

--- Rebar/RebarStair/View/RebarStairView.xaml.cs
+++ Rebar/RebarStair/View/RebarStairView.xaml.cs
@@ -10,19 +10,20 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Rebar
+namespace Rebar.RebarStair.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for RebarStairView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class RebarStairView : Window
     {
-        public MainWindow()
+        public RebarStairView()
         {
             InitializeComponent();
         }
+
+       
     }
 }
